ad3e5a1a83b3191017f71da2b16416c979c12157
JonasRocha189 <jonas.rocha189@gmail.com>
2023-10-04 07:46:28 -0300

    get URL from HTML and tests

--- crawl.test.js
+++ crawl.test.js
@@ -1,4 +1,4 @@
-const { normalizeURL } = require('./crawl.js')
+const { normalizeURL, getURLsfromHTML } = require('./crawl.js')
 const { test, expect } = require('@jest/globals')
 
 test('normalizeURL strip protocol', () => {
@@ -26,5 +26,41 @@ test('normalizeURL strip http', () => {
   const input = 'http://blog.boot.dev/path/'
   const actual = normalizeURL(input)
   const expected = 'blog.boot.dev/path'
+  expect(actual).toEqual(expected)
+})
+
+test('getURLsFromHTM absolute', () => {
+  const inputHTMLBody = `<html>
+      <body>
+        <a href="https://blog.boot.dev">Boot.dev Blog</a>
+      </body>
+    </html>`
+  const inputBaseURL = 'https://blog.boot.dev'
+  const actual = getURLsfromHTML(inputHTMLBody, inputBaseURL)
+  const expected = ['https://blog.boot.dev/']
+  expect(actual).toEqual(expected)
+})
+
+test('getURLsFromHTM relative', () => {
+  const inputHTMLBody = `<html>
+      <body>
+        <a href="/path/">Boot.dev Blog</a>
+      </body>
+    </html>`
+  const inputBaseURL = 'https://blog.boot.dev'
+  const actual = getURLsfromHTML(inputHTMLBody, inputBaseURL)
+  const expected = ['https://blog.boot.dev/path/']
+  expect(actual).toEqual(expected)
+})
+
+test('getURLsFromHTM invalid', () => {
+  const inputHTMLBody = `<html>
+      <body>
+        <a href="invalid">Invalid URL</a>
+      </body>
+    </html>`
+  const inputBaseURL = 'https://blog.boot.dev'
+  const actual = getURLsfromHTML(inputHTMLBody, inputBaseURL)
+  const expected = []
   expect(actual).toEqual(expected)
 })
